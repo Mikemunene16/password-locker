dd3083637478912661d7e4928ed11e866394483c
Mike Munene <mike.munene@moringaschool.com>
2021-06-03 13:09:37 +0300

    Create class Credentials and method to verify users

--- passlocker.py
+++ passlocker.py
@@ -22,6 +22,42 @@ class User:
         """
 
         User.user_list.append(self) 
-        
-           
+
+    @classmethod
+    def display_user(cls):
+        return cls.user_list
+
+
+    def delete_user(self):
+        """
+        A method that deletes a saved account from the user list
+        """
+
+        User.user_list.remove(self)    
+
+
+
+class Credentials():
+    """
+    Create Credentials class to generate and verify user credentials.
+    """
+
+    credentials_list = []
+
+    @classmethod
+    def verify_user(cls, username, password):
+        """
+        A method to verify whether the user is in the User list or not.
+        """
+
+        a_user = ""
+        for user in User.user_list:
+            if(user.username == username and user.password == password):
+                a_user == user.username
+
+        return a_user
+            
+
+
+
 
